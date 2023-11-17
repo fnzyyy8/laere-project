ac3b3b690ac9f97ba8f793d090871c5ab24e6dfc
fnzyyy8 <farhanshidayat@gmail.com>
2023-11-17 16:56:49 +0700

    feat: HomePages and nav

--- app/homeSection/Hero.tsx
+++ app/homeSection/Hero.tsx
@@ -1,6 +1,16 @@
 import style from "../home.module.css"
 export default function Hero() {
     return (
-        <section className={style.hero}>Hero</section>
+        <section className={style.hero}>
+            <div className={style.content_hero}>
+                <div className={style.tagline}>
+                    <h1>Digitalisasi</h1>
+                    <h1>Tanpa Batasan</h1>
+                </div>
+                <p>Percayakan persona utama perusahaan anda bersama kami. Laere digitalisasi bebas tanpa batas.</p>
+            </div>
+
+
+        </section>
     )
 }
